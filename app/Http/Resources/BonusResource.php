add7870229f657889a909e5dbaa92ba8b58a5d0d
simdihavale <simdihavale@protonmail.me>
2024-04-30 13:35:37 +0300

    resource eklendi

--- app/Http/Resources/BonusResource.php
+++ app/Http/Resources/BonusResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Blog extends JsonResource
+class BonusResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Bonus extends JsonResource
+class PaymentResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
