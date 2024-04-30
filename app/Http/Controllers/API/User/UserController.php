1a62a8e01f8734f9e41f3b47ded0b72da9741773
simdihavale <simdihavale@protonmail.me>
2024-04-30 12:28:56 +0300

    ruute edit

--- app/Http/Controllers/API/User/UserController.php
+++ app/Http/Controllers/API/User/UserController.php
@@ -1,11 +1,22 @@
 <?php
-
-namespace App\Http\Controllers\User;
-
-use App\Http\Controllers\Controller;
+   
+namespace App\Http\Controllers\API;
+   
 use Illuminate\Http\Request;
-
-class User extends Controller
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\Product;
+use Validator;
+use App\Http\Resources\ProductResource;
+use Illuminate\Http\JsonResponse;
+   
+class UserController extends BaseController
 {
-    //
+ 
+    public function index(): JsonResponse
+    {
+        $products = Product::all();
+       
+        return $this->sendResponse(ProductResource::collection($products), 'Products retrieved successfully.');
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,47 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\API\RegisterController;
 use App\Http\Controllers\API\ProductController;
+use App\Http\Controllers\API\User\UserController;
 
-Route::controller(RegisterController::class)->group(function(){
+Route::controller(RegisterController::class)->group(function () {
     Route::post('register', 'register');
     Route::post('login', 'login');
 });
-        
-Route::middleware('auth:sanctum')->group( function () {
-   // Route::resource('products', ProductController::class);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('products', ProductController::class);
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('user/list', UserController::class);
+    Route::resource('user/balance', UserController::class);
+    Route::resource('user/activites', UserController::class);
+    Route::resource('user/details', UserController::class);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('bonus/list', UserController::class);
+    Route::resource('bonus/categori', UserController::class);
+    Route::resource('bonus/requests', UserController::class);
+    Route::resource('bonus/logsa', UserController::class);
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('blog/category', UserController::class);
+    Route::resource('blog/content', UserController::class);
+    Route::resource('bonus/settings', UserController::class);
+    Route::resource('bonus/list', UserController::class);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('game/provider', UserController::class);
+    Route::resource('game/list', UserController::class);
+    Route::resource('game/category', UserController::class);
+});
+
+
+
+
