5a17ef3e8963368aa9b9bbbb830a9c2fe4f332ae
simdihavale <simdihavale@protonmail.me>
2024-04-30 11:55:10 +0300

    route değişim

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
 
-Route::get('/usertest', function () {
-    echo 1;
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+        
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
 });
