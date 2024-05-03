efe294579db87f4bed15eb185c34202bb35cbc10
simdihavale <simdihavale@protonmail.me>
2024-05-03 13:57:16 +0300

    user notfications

--- app/Http/Controllers/API/User/NotficationsController.php
+++ app/Http/Controllers/API/User/NotficationsController.php
@@ -1,11 +1,83 @@
+
+
 <?php
 
-namespace App\Http\Controllers\User;
+namespace App\Http\Controllers\API\User;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\user_notifications;
+use Validator;
+use App\Http\Resources\UserNotficationsResource;
+use Illuminate\Http\JsonResponse;
 
-class Notfications extends Controller
-{
-    //
+class UserNotfications extends BaseController {
+
+    /**
+     * 
+     * @return type
+     */
+    public function index() {
+        $dataClass = User::all();
+        return $this->sendResponse(UserResource::collection($dataClass), 'User retrieved successfully.');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request) {
+        $input = $request->all();
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id): JsonResponse {
+
+        $product = User::find($id);
+
+        if (is_null($product)) {
+            return $this->sendError('Product not found.');
+        }
+
+        return $this->sendResponse(new UserResource($product), 'User retrieved successfully.');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id) {
+        //
+    }
+
+    /**
+     * 
+     * @param type $param
+     */
+    public function clears($param) {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id) {
+        $input = $request->all();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id) {
+        $id->delete();
+
+        return $this->sendResponse([], 'Product deleted successfully.');
+    }
 }
