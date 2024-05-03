eb587e9f5e7eeb2d8b3e6992e2fd423db4865baa
simdihavale <simdihavale@protonmail.me>
2024-05-03 13:32:13 +0300

    command user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,8 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'detail' => $this->detail,
+            'email' => $this->email,
+            'remember_token'=> $this->remember_token,
             'created_at' => $this->created_at->format('d/m/Y'),
             'updated_at' => $this->updated_at->format('d/m/Y'),
         ];
